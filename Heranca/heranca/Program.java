2d7851ed6cb117dff440a18d66c8291faccc92ca
dersolopes <anderson.ribeiro.lopes@gmail.com>
2025-08-01 14:32:29 -0300

    Corrigindo a saida do exercicio que estava errada no enunciado

--- Heranca/heranca/Program.java
+++ Heranca/heranca/Program.java
@@ -7,7 +7,12 @@ public class Program {
 
         System.out.println("meuAnimal eh um " + meuAnimal.getClass().getName());
         System.out.println("meuCachorro eh um " + meuCachorro.getClass().getName());
-        System.out.println("objeto eh um " + obj.getClass().getName());
+        System.out.println("obj eh um " + obj.getClass().getName());
         System.out.println("umOutroAnimal eh um " + umOutroAnimal.getClass().getName());
     }
 }
+
+    meuAnimal eh um Animal
+    meuCachorro eh um Cachorro
+    objeto eh um Cachorro
+    umOutroAnimal eh um Cachorro

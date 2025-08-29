40fa55738fb85c580ed11a895490e9e4b25cbb7b
dersonlopes <and_lopes@live.com>
2025-08-29 12:59:41 -0300

    Java - Orientação a Objetos - Exercicio 2. Métodos

---
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -0,0 +1,44 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+
+
+
+    //metodos
+    public void tomarDano(int quantidadeDeDano){
+
+        if (saudeAtual >= 0 && quantidadeDeDano <= saudeAtual) { // Caso o dano seja inferior a saude, o personagem tomara o dano
+        this.saudeAtual -= quantidadeDeDano;
+        } else if (saudeAtual >= 0 && quantidadeDeDano >= saudeAtual){ // Caso o dano seja superior a saude, a saude ira para zero
+            this.saudeAtual = 0;
+        }
+    }
+
+    public void receberCura (int cura) {
+
+        if ((saudeAtual + cura) <= 100) {
+            this.saudeAtual += cura;
+        } else if ((saudeAtual + cura) >= 100) {
+            this.saudeAtual = 100;
+        }
+    }
+
+    // Getters and Setters
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+
+}

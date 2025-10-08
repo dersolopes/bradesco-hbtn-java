0f8faa3cd92d7a0145b59387138b8cfe87502bac
dersonlopes <and_lopes@live.com>
2025-10-08 13:08:58 -0300

    Java - Collectors - Exercicio 1. Idades das pessoas agrupadas por cargo

--- 0x19/idade_por_cargo/ConsultaPessoas.java
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -11,4 +11,16 @@ public class ConsultaPessoas {
                 //  - chave: nome do cargo (String)
                 //  - valor: lista de pessoas com aquele cargo (List<Pessoa>)
     }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> todasPessoas) {
+
+        return todasPessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, // agrupa os elementos pelo cargo
+                        Collectors.mapping(Pessoa::getIdade, Collectors.toList()) // transforma cada Pessoa do grupo em sua idade
+                ));
+        //  O resultado é um Map<String, List<Integer>>:
+        //      chave: nome do cargo
+        //      valor: lista das idades das pessoas com aquele cargo
+    }
+
 }

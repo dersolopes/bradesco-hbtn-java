358f556b9c85df76ac3644f8f8a8f4a8930ae56f
dersonlopes <and_lopes@live.com>
2025-10-08 14:10:17 -0300

    Java - Collectors - Exercicio 2. Pessoas agrupadas por cargo e que possuam pelo menos 40 anos

---
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> todasPessoas) {
+
+        return todasPessoas.stream() // transforma a lista em um fluxo de objetos Pessoa.
+                .collect(Collectors.groupingBy(Pessoa::getCargo)); // agrupa todas as pessoas que têm o mesmo valor retornado por getCargo() (ou seja, o nome do cargo).
+                // O resultado é um Map<String, List<Pessoa>>:
+                //  - chave: nome do cargo (String)
+                //  - valor: lista de pessoas com aquele cargo (List<Pessoa>)
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> todasPessoas) {
+
+        return todasPessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, // agrupa os elementos pelo cargo
+                        Collectors.mapping(Pessoa::getIdade, Collectors.toList()) // transforma cada Pessoa do grupo em sua idade
+                ));
+        //  O resultado é um Map<String, List<Integer>>:
+        //      chave: nome do cargo
+        //      valor: lista das idades das pessoas com aquele cargo
+    }
+
+    // Agrupa pessoas por cargo e filtra idade > 40
+    public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        Collectors.filtering(p -> p.getIdade() > 40, Collectors.toList())
+                ));
+        //  O resultado é um Map<String, List<Integer>>:
+        //      chave: nome do cargo
+        //      valor: lista de pessoas daquele cargo e que possuam idade acima de 40 anos como valor do Map
+    }
+}

dadb1925f78082caa5f9d689cce4383ffb253b3e
dersonlopes <and_lopes@live.com>
2025-10-09 18:00:56 -0300

    Java - Streams, Collectors e Optional - Exercícios de fixação - Exercicio 3. Média salarial por cargo

---
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo, // Agrupa pessoas pelo cargo.
+                        () -> new TreeMap<>(Comparator.reverseOrder()), // ordem reversa dos cargos
+                        Collectors.toCollection(TreeSet::new) //// Cada grupo é armazenado em TreeSet, 'pessoas' ordenadas pelo compareTo
+                ));
+    }
+
+    // Retorna um Map contendo o nome do cargo como chave
+    // e a quantidade de pessoas daquele cargo como valor.
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo, // Agrupa pessoas pelo cargo.
+                        Collectors.counting() // conta quantas há em cada grupo.
+                ));
+    }
+
+    // Retorna um Map de Cargo contendo outro Map aninhado com
+    // Idade e quantidade de pessoas com esta idade
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo, // Agrupa pessoas pelo cargo.
+                        Collectors.groupingBy(Pessoa::getIdade,Collectors.counting()) //  idade e quantidade de pessoas daquela idade.
+                ));
+    }
+
+    //Retorna um Map<String, Double> que possua o Cargo e o salário médio daquele cargo.
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo, // Agrupa pessoas pelo cargo.
+                        Collectors.averagingDouble(Pessoa::getSalario) // salário médio daquele cargo.
+                ));
+    }
+
+}

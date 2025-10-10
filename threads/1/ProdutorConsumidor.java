b2f0d241eeb6eb12e6561fa730d41d1d0890334c
Anderson Ribeiro Lopes <anderson.r.lopes@bradesco.com.br>
2025-10-10 18:22:16 -0300

    Java Multithreading - Exercicio 1. Notificações em Java (versao ajustada)

--- threads/1/ProdutorConsumidor.java
+++ threads/1/ProdutorConsumidor.java
@@ -4,7 +4,10 @@ public class ProdutorConsumidor {
 
 
         // Criar e iniciar as threads do produtor e consumidor
+        Produtor produtor = new Produtor(buffer); //Criando a thread do produtor
+        Consumidor consumidor = new Consumidor(buffer); //Criando a thread do consumidor
 
-
+        produtor.start(); //Iniciando a thread do produtor
+        consumidor.start(); //Iniciando a thread do consumidor
     }
 }
